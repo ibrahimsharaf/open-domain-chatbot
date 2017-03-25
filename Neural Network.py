60c7891798ee8c7585f595df0181c3c434333afe
Abdelrahman Radwan <abdelrahman_hamdy_radwan@yahoo.com>
2017-03-25 20:44:34 +0200

    Merge the work, start fixing Sharaf notes, Start adding the Neural Cell function usage, and step function

--- Neural Network.py
+++ Neural Network.py
@@ -1,10 +1,40 @@
 import tensorflow as tf
 
-x = tf.constant(35, name='x')
-y = tf.Variable(x + 5, name='y')
 
-model = tf.initialize_all_variables()
 
-sess = tf.Session()
-sess.run(model)
-print(sess.run(y))
+class Model:
+    """
+    Implementation of a seq2seq model.
+    Architecture:
+        Encoder/decoder
+        2 LTSM layers
+    """
+    def __init__(self, args, textData):
+        """
+        Args:
+            args: parameters of the model
+            textData: the dataset object
+        """
+        print("Model creation...")
+
+        self.textData = textData  # Keep a reference on the dataset
+        self.args = args  # Keep track of the parameters of the model
+        self.dtype = tf.float32
+
+        # Placeholders
+        self.encoderInputs  = None
+        self.decoderInputs  = None  # Same that decoderTarget plus the <go>
+        self.decoderTargets = None
+        self.decoderWeights = None  # Adjust the learning to the target sentence size
+
+        # Main operators
+        self.lossFct = None
+        self.optOp = None
+        self.outputs = None  # Outputs of the network, list of probability for each words
+
+        # Construct the graphs
+        self.buildNetwork()
+
+
+    def buildNetwork(self):
+        print("ahda 3laya");
